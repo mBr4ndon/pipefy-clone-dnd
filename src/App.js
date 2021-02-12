0a80ea2f98c58ed666d84c5a5bb558cbf1a23f6c
Marcio Brandao <brandao.marcio.xix@gmail.com>
2021-02-12 19:24:20 +0000

    :tada: Pipefy done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { DndProvider } from 'react-dnd'
+import {HTML5Backend} from 'react-dnd-html5-backend'
+
+import Header from './components/Header'
+import Board from './components/Board'
+import GlobalStyle from './styles/global'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <Header />
+      <Board />
+
+      <GlobalStyle />
+    </DndProvider>
   );
 }
 
